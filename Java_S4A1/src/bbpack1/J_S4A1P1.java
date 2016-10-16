d82077c897d812957f22a203ef725751e0006dc4
BrijeshParambikadan <bbavittom@gmail.com>
2016-10-16 18:22:51 +0530

    submitted JavaS4A1 assignment

---
+++ Java_S4A1/src/bbpack1/J_S4A1P1.java
@@ -0,0 +1,10 @@
+package bbpack1; // package bbpack1
+
+public class J_S4A1P1
+{
+protected void display(){
+
+	System.out.println("I belong to Package bbpack1");
+
+}
+}
---
+++ Java_S4A1/src/bbpack2/J_S4A1P2.java
@@ -0,0 +1,12 @@
+package bbpack2; // package bbpack2
+
+public class J_S4A1P2 extends bbpack1.J_S4A1P1{
+	
+	public void show(){
+	
+		new J_S4A1P2().display();//is working
+
+	}
+	}
+
+//Thank You
